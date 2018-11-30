0e1a33edf21115d4455a0ef9a8ea2f264deafdfb
waldner <waldner@katamail.com>
2018-11-30 20:09:11 +0100

    use --paperkey switch instead of -p (silliest backward-incompatible change ever)

--- run_keybase.sh
+++ run_keybase.sh
@@ -2,7 +2,7 @@
 
 trap 'fusermount -u ${KEYBASE_KBFS_MOUNT}' SIGINT SIGTERM
 
-keybase oneshot -u "${KEYBASE_USER}" -p "${KEYBASE_PAPERKEY}"
+keybase oneshot -u "${KEYBASE_USER}" --paperkey "${KEYBASE_PAPERKEY}"
 
 keybase config set mountdir "${KEYBASE_KBFS_MOUNT}"
 
